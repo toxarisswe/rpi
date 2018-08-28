af5943596c3df454149bcba8a703b4b056e7b8b1
Daniel Persson <toxaris@hotmail.com>
2018-08-28 23:17:00 +0200

    Add existing file

---
+++ GPIO_01.py
@@ -0,0 +1,16 @@
+#!/user/bin/emv python
+import RPi.GPIO as GPIO
+import time
+import random
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+
+while True:
+    GPIO.output(11, True)
+    randtime = random.uniform(0.1,0.9)
+    time.sleep((randtime))
+    GPIO.output(11, False)
+    randtime = random.uniform(0.1,0.9)
+    time.sleep((randtime))
